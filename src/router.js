1b7abb6a03310f379616aaa3ad0f79db1ffa5783
banglh <banglh@jienielab.asia>
2021-06-08 21:30:29 +0700

    change mode to hash

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: "history",
+  mode: "hash",
   scrollBehavior() {
     return { x: 0, y: 0 };
   },
